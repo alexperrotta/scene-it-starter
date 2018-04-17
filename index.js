faed067647f94c3058c7db7620445699759a9a15
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-04-17 19:48:08 -0400

    part 2 changes

--- index.js
+++ index.js
@@ -15,13 +15,31 @@ $(function(){
 			finalHTML += '<div class="card-body">';
 			finalHTML += '<h5 class="card-title">'+ currentMovie.Title +'</h5>';
 			finalHTML += '<p class="card-text">'+ currentMovie.Year +'</p>';
-			finalHTML += '<button class="btn btn-primary">Add</button>';
+			finalHTML += '<button data-id="'+currentMovie.imdbID +'" class="btn btn-primary">Add</button>';
 			finalHTML += '</div>';
 			finalHTML += '</div>';
 		});
 		return finalHTML;
 	}
 
+
+	// Setting up the click listener on the movies
+	$('.movies-container').on('click', 'btn', function(){
+		var imdbID = $(this).data('id');
+		var movie = movieData.find(function(currentMovie){
+			return currentMovie.imdbID == imdbID;
+		});
+		
+		var watchlistJSON = localStorage.getItem('watchlist');
+		var watchlist = JSON.parse(watchlistJSON);
+		if (watchlist = null) {
+			watchlist = {};
+		}
+		
+		watchlist.push(movie);
+		watchlistJSON = JSON.stringify(watchlist);
+		localStorage.setItem('watchlist', watchlistJSON);
+	});
 
 
 
